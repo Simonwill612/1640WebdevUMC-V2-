c485e26bd210a9b66a093caf83b6df5761afbe8c
SHIELD MALER <simonwill@outlook.com.vn>
2024-03-18 18:02:53 +0700

    Add project files.

---
+++ 1640WebdevUMC/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using _1640WebDevUMC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace _1640WebDevUMC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AcademicYear>? AcademicYears { get; set; }
+        public DbSet<Contribution>? Contributions { get; set; }
+        public DbSet<Faculty>? Faculties { get; set; }
+        public DbSet<DownloadHistory>? DownloadHistories { get; set; }
+        public DbSet<_1640WebDevUMC.Models.File>? Files { get; set; } // Fully qualify the namespace to resolve ambiguity
+        public DbSet<Image>? Images { get; set; }
+        public DbSet<Notification>? Notifications { get; set; }
+        public DbSet<Accounts>? Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure keyless entity type for SelectListItem
+            modelBuilder.Entity<SelectListGroup>().HasNoKey();
+        }
+    }
+}
---
+++ 1640WebdevUMC/Program.cs
@@ -0,0 +1,52 @@
+using _1640WebDevUMC.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using _1640WebDevUMC.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+    options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
